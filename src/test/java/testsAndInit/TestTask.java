b9e7f12df18cd8fb1f3b79660d974c2900443b77
Vladimir774400 <stanly744@yandex.ru>
2022-08-14 20:47:27 +0300

    Comments

--- src/test/java/testsAndInit/TestTask.java
+++ src/test/java/testsAndInit/TestTask.java
@@ -6,6 +6,7 @@ import pageObjects.MainPageObject;
 
 
 public class TestTask extends testsAndInit.Initialize{
+    //All mandatory empty fields should have error description displayed after ‘Continue to checkout’ is pressed
     @Test
     public void testErrFieldsAppears() {
         MainPageObject mainPageObject = new MainPageObject(driver);
@@ -13,12 +14,14 @@ public class TestTask extends testsAndInit.Initialize{
         mainPageObject.allFieldsErrVisibilityCheck();
     }
 
+    //‘Cart’ successfully loaded when user opens the page;
     @Test
     public void testCartSuccessfullyLoad() {
         MainPageObject mainPageObject = new MainPageObject(driver);
         mainPageObject.waitCartLoad();
     }
 
+    //Promo code correctly changes ‘Total’ field
     @Test
     public void testPromoCodeCalculatedCorrectly() {
         MainPageObject mainPageObject = new MainPageObject(driver);
@@ -26,6 +29,7 @@ public class TestTask extends testsAndInit.Initialize{
         mainPageObject.checkTotalAfterApplyingPromoCode("1234567891");
     }
 
+    //‘Your order was placed’ displayed after form submission
     @Test
     public void testOrderSubmitCheck() {
         MainPageObject mainPageObject = new MainPageObject(driver);
@@ -34,12 +38,14 @@ public class TestTask extends testsAndInit.Initialize{
         mainPageObject.successTextAppears();
     }
 
+    //API correctly calculates discount percent
     @Test
     public void testApiCorrectDiscountAmount() {
         MainPageObject mainPageObject = new MainPageObject(driver);
         mainPageObject.correctDisc("promocode");
     }
 
+    //API returns error when credit card number length not equals 16
     @Test
     public void testApiErrorWithCartNumLenth() {
         MainPageObject mainPageObject = new MainPageObject(driver);
