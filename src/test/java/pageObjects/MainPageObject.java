4bae7c1ba906757f8a00c2c55be2995963503982
avklimakina <avklimakina@ya.ru>
2022-12-30 15:03:08 +0400

    changes

--- src/test/java/pageObjects/MainPageObject.java
+++ src/test/java/pageObjects/MainPageObject.java
@@ -1,7 +1,8 @@
 package pageObjects;
 
-import io.restassured.RestAssured;
-import org.junit.Assert;
+import io.qameta.allure.Step;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -12,7 +13,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MainPageObject {
     protected ChromeDriver driver;
@@ -21,13 +23,15 @@ public class MainPageObject {
         this.driver = driver;
     }
 
-    //Локаторы
-    private static final String
+    public static final String
             ELEMENT_CONTAINS_TEXT = "//*[contains(text(), '{SUBSTRING}')]",
             URL_STARTS_WITH = "a[href^='{SUBSTRING}']",
             SEARCH_WITH_PARAM = "[{PARAM}='{VALUE}']",
             SEARCH_PAGES = "[aria-label='Page {PAGE_NUM}']",
             FIELDS_WITH_ERROR = "[class='invalid-feedback']",
+            GOOGLE_SEARCH_FIELD = "[class='gLFyf']",
+            GOOGLE_SEARCH_BUTTON = "[class='gNO89b']",
+            GOOGLE_SEARCH_TITLES = "[class='LC20lb MBeuO DKV0Md']",
             CART_LOADING = "[id='loading']",
             TOTAL_AMOUNT = "[id='totalAmount']",
             PROMO_CODE_FIELD = "[id='promoCode']",
@@ -40,87 +44,27 @@ public class MainPageObject {
             SUCCESS_MESS = "[id='success']";
 
 
-    //API returns error when credit card number length not equals 16 method
-    public void incorrectCardNumLenghtCheck(String cart_num)
-    {
-        RestAssured.baseURI = "https://fg1ap986e9.execute-api.eu-west-1.amazonaws.com/Dev";
-        given()
-                .header("Content-Type", "application/json")
-                .body("{\n" +
-                        "    \"firstName\": \"Vlad\",\n" +
-                        "    \"lastName\": \"etdfs\",\n" +
-                        "    \"paymentMethod\": \"dsfd\",\n" +
-                        "    \"cc-name\": \"dsfdf sdfsf\",\n" +
-                        "    \"cc-number\": \""+cart_num+"\",\n" +
-                        "    \"ccexpiration\": \"21.12\",\n" +
-                        "    \"cc-cvv\": \"121\"\n" +
-                        "}")
-                .when()
-                .post("/checkout")
-                .then()
-                .assertThat()
-                .body("message", equalTo("Invalid Card Number"));
+    @Step("POST - {0}")
+    public Response sendPostRequest(String url, String body, int expectedStatusCode) {
+        Response resp = given()
+                .contentType(ContentType.JSON)
+                .body(body)
+                .post(url);
+
+        assertEquals(String.valueOf(expectedStatusCode), String.valueOf(resp.getStatusCode()), "Unexpected status code " + resp.statusCode() + ". Response was " + resp.body().asString());
+        return resp;
     }
 
-    //API correctly calculates discount percent method
-    public void correctDisc(String promo_code) {
-        int discount_amount = promo_code.length();
-        Assert.assertTrue("Promo code should be less than 100 symbols",promo_code.length()<100);
-        RestAssured.baseURI = "https://fg1ap986e9.execute-api.eu-west-1.amazonaws.com/Dev";
-        given()
-                .header("Content-Type", "application/json")
-                .when()
-                .post("/coupon?coupon=" + promo_code)
-                .then()
-                .assertThat()
-                .body("discount", equalTo(discount_amount));
+    @Step("GET - {0}")
+    public Response sendGetRequest(String url, int expectedStatusCode) {
+        Response resp = given()
+                .contentType(ContentType.JSON)
+                .get(url);
+
+        assertEquals(String.valueOf(expectedStatusCode), String.valueOf(resp.getStatusCode()), "Unexpected status code " + resp.statusCode() + ". Response was " + resp.body().asString());
+        return resp;
     }
 
-    //Fill all fields with correct data
-    public void fillAllFields() {
-        waitForElementAndTypeText("Johny", By.cssSelector(FIRST_NAME_FIELD), "First name field NOT appears", 15);
-        waitForElementAndTypeText("Depp", By.cssSelector(LAST_NAME_FIELD), "Last name field NOT appears", 15);
-        waitForElementAndTypeText("Johny Depp", By.cssSelector(CART_NAME_FIELD), "Cart name field NOT appears", 15);
-        waitForElementAndTypeText("1234567891011121", By.cssSelector(CART_NUM_FIELD), "Cart number field NOT appears", 15);
-        waitForElementAndTypeText("08.25", By.cssSelector(CART_EXP_FIELD), "Cart exp date field NOT appears", 15);
-        waitForElementAndTypeText("123", By.cssSelector(CART_CCV_FIELD), "CCV field NOT appears", 15);
-    }
-
-    //Success text appears check after form submission method
-    public void successTextAppears() {
-        waitForElementPresent(By.cssSelector(SUCCESS_MESS), "Success text NOT appears", 15);
-    }
-
-    //Promo code changes Total check
-    public void checkTotalAfterApplyingPromoCode(String promo_code) {
-        int discount_amount = promo_code.length();
-        String b = driver.findElement(By.cssSelector(TOTAL_AMOUNT)).getText();
-        waitForElementAndTypeText(promo_code, By.cssSelector(PROMO_CODE_FIELD), "Promo code field was not detected", 15);
-        waitElementContainsTextAndClick("Redeem");
-        String a = driver.findElement(By.cssSelector(TOTAL_AMOUNT)).getText();
-
-        float before_apply = Float.parseFloat(b);
-        float after_apply = Float.parseFloat(a);
-        float exp_float = before_apply - ((before_apply * discount_amount) / 100);
-        Assert.assertTrue("Total amount after promo code apply should be '" + exp_float + "'", exp_float == after_apply);
-    }
-
-    //Fields error message visibility method
-    public void allFieldsErrVisibilityCheck() {
-        List<WebElement> errs = new ArrayList<>();
-        errs = driver.findElements(By.cssSelector(FIELDS_WITH_ERROR));
-        for (int i = 0; i < errs.size() - 1; i++) {
-            Assert.assertTrue("Not all field has an error message", errs.get(i).isDisplayed());
-        }
-    }
-
-    //Wait until cart load method
-    public void waitCartLoad() {
-        waitForElementNotPresent(By.cssSelector(CART_LOADING), "Cart was not loaded within 15 seconds", 15);
-    }
-
-
-    //Метод ожидания элемента
     public WebElement waitForElementPresent(By by, String err_mes, long timeoutInSec) {
         WebDriverWait wait = new WebDriverWait(driver, timeoutInSec);
         wait.withMessage(err_mes + "\n");
@@ -128,7 +72,6 @@ public class MainPageObject {
         return wait.until(ExpectedConditions.presenceOfElementLocated(by));
     }
 
-    //Метод ожидания исчезания элемента
     public boolean waitForElementNotPresent(By by, String err_mes, long timeoutInSec) {
         WebDriverWait wait = new WebDriverWait(driver, timeoutInSec);
         wait.withMessage(err_mes + "\n");
@@ -136,107 +79,77 @@ public class MainPageObject {
         return wait.until(ExpectedConditions.invisibilityOfElementLocated(by));
     }
 
-    //Метод ввода в поисковую строку google определенного текста
     public void waitForElementAndTypeText(String text, By by, String err_mes, long timeoutInSec) {
         WebElement field = waitForElementPresent(by, err_mes, timeoutInSec);
+        field.click();
         field.clear();
         field.sendKeys(text);
     }
 
-    //Метод ввода в поле с плейсхолдером определенного текста
-    public void waitForElemenWithTextAndTypeText(String searched_text, String type_text) {
-        WebElement element = waitForElementPresent(By.id("searchBox"), "Не найден элемент поиска", 15);
-        element.click();
-        element.clear();
-        element.sendKeys(type_text);
-    }
-
-    //Метод нажатия на кнопку
+    @Step("Wait for element {0} and click")
     public void waitForElementPresentAndClick(By by, String err_mes, long timeoutInSec) {
         waitForElementPresent(by, err_mes, timeoutInSec).click();
     }
 
-    //Метод поиска и клика элемента содержащего текст
+    @Step("Click on {0} Google search result")
+    public void clickOnSearchResult(int num) throws InterruptedException {
+        List<WebElement> titlesWebEl = driver.findElements(By.cssSelector(GOOGLE_SEARCH_TITLES));
+        titlesWebEl.get(num).click();
+        Thread.sleep(5_000);
+    }
+
+    @Step("Search in Google method")
+    public void searchInGoogle(String text, long timeoutInSec) {
+        waitForElementAndTypeText(text, By.cssSelector(GOOGLE_SEARCH_FIELD),"Google search field not found",timeoutInSec);
+        waitForElementPresentAndClick(By.cssSelector(GOOGLE_SEARCH_BUTTON),"Google search bttn not found",timeoutInSec);
+        List<WebElement> titlesWebEl = driver.findElements(By.cssSelector(GOOGLE_SEARCH_TITLES));
+        List<String> titles = new ArrayList<>();
+        titlesWebEl.stream().forEach(title -> titles.add(title.getAttribute("innerText")));
+        assertTrue(titles.stream().anyMatch(title -> title.contains(text)), "There is no relevant search results");
+    }
+
+    @Step("Wait for element contains text {0} and click")
     public void waitElementContainsTextAndClick(String text) {
         String searchedText = ELEMENT_CONTAINS_TEXT.replace("{SUBSTRING}", text);
-        WebElement element = waitForElementPresent(By.xpath(searchedText), "Не найдено элементов с текстом " + text, 15);
+        WebElement element = waitForElementPresent(By.xpath(searchedText), "No element with text " + text, 15);
         element.click();
     }
 
-    //Метод поиска элемента содержащего текст
+    @Step("Wait for element contains text {0}")
     public void waitElementContainsText(String text) {
         String searchedText = ELEMENT_CONTAINS_TEXT.replace("{SUBSTRING}", text);
-        WebElement element = waitForElementPresent(By.xpath(searchedText), "Не найдено элементов с текстом " + text, 15);
+        WebElement element = waitForElementPresent(By.xpath(searchedText), "No element with text " + text, 15);
     }
 
-    //Метод поиска элемента содержащего текст
+    @Step("Wait for element contains text {0}, boolean")
     public boolean waitElementContainsTextAssertion(String text) {
         String searchedText = ELEMENT_CONTAINS_TEXT.replace("{SUBSTRING}", text);
-        WebElement element = waitForElementPresent(By.xpath(searchedText), "Не найдено элементов с текстом " + text, 15);
+        WebElement element = waitForElementPresent(By.xpath(searchedText), "No element with text " + text, 15);
         return element.isDisplayed();
     }
 
-    //Метод поиска элемента определенный параметр и значение
+    @Step("Search element with param {0} which equals {1}")
     public void searchWithParam(String param, String value) {
         String loc = SEARCH_WITH_PARAM.replace("{PARAM}", param).replace("{VALUE}", value);
-        WebElement element = waitForElementPresent(By.cssSelector(loc), "Не найдено элементов с параметром '" + param + "' и значением '" + value + "'", 15);
+        WebElement element = waitForElementPresent(By.cssSelector(loc), "No element with '" + param + "' and value '" + value + "'", 15);
     }
 
-    //Метод возврата появления элемента содержащего текст
-    public boolean searchWithParamAssertion(String param, String value) {
-        String loc = SEARCH_WITH_PARAM.replace("{PARAM}", param).replace("{VALUE}", value);
-        WebElement element = waitForElementPresent(By.cssSelector(loc), "Не найдено LIVE рейсов", 15);
-        return element.isDisplayed();
-    }
-
-    //Метод поиска элемента по классу
+    @Step("Wait for element with class equals {0}")
     public void waitElementByClass(String class_name) {
-        WebElement element = waitForElementPresent(By.cssSelector(class_name), "Не найдено элементов с class " + class_name, 15);
+        WebElement element = waitForElementPresent(By.cssSelector(class_name), "No element with class " + class_name, 15);
     }
 
-    //Метод возврата элемента содержащего текст
+    @Step("Return element contains text {0}")
     public WebElement returnElementContainsText(String text) {
         String searchedText = ELEMENT_CONTAINS_TEXT.replace("{SUBSTRING}", text);
-        WebElement element = waitForElementPresent(By.xpath(searchedText), "Не найдено элементов с текстом " + text, 15);
+        WebElement element = waitForElementPresent(By.xpath(searchedText), "No element with text " + text, 15);
         return element;
     }
 
-    //Метод находит все элементы c URL начинающимся на
+    @Step("Return all elements with URL starts with {0}")
     public List<WebElement> findAllElementsWithUrlStartsWith(String text) {
         String searchedUrl = URL_STARTS_WITH.replace("{SUBSTRING}", text);
         List<WebElement> elements = driver.findElements(By.cssSelector(searchedUrl));
         return elements;
     }
-
-    //Метод перехода на определенную страницу поиска
-    public void goToPage(Integer pageNum) {
-        String num = pageNum.toString();
-        String pageXpath = SEARCH_PAGES.replace("{PAGE_NUM}", num);
-        WebElement element = waitForElementPresent(By.cssSelector(pageXpath), "Не найдено номера страницы '" + pageNum + "'", 15);
-        element.click();
-    }
-
-    //Метод перехода к мониторингу определенного live рейса
-    public void goToLiveFlight(String param, String value) {
-        String loc = SEARCH_WITH_PARAM.replace("{PARAM}", param).replace("{VALUE}", value);
-        WebElement element = waitForElementPresent(By.cssSelector(loc), "Не найдено элементов с параметром '" + param + "' и значением '" + value + "'", 15);
-        if (element.isDisplayed()) {
-            List<WebElement> lis = driver.findElements(By.cssSelector(loc));
-            lis.get(0).click();
-        }
-    }
-
-    //Метод поиска определенного live рейса на flightRadar
-    public void searchForFlight(String flt) {
-        waitElementContainsTextAndClick("Flightradar24: Live Flight Tracker");
-        if (waitElementContainsTextAssertion("Accept Cookies")) {
-            waitElementContainsTextAndClick("Accept Cookies");
-        }
-        System.out.println("Ищу рейс '" + flt + "'");
-        waitForElemenWithTextAndTypeText("Search", flt);
-        boolean is_live = searchWithParamAssertion("data-testid", "search__result-live__go-to-live");
-        Assert.assertTrue("Рейс '" + flt + "' не в воздухе", is_live);
-        goToLiveFlight("data-testid", "search__result-live__go-to-live");
-    }
-
 }
---
+++ src/test/java/tests/FirstTest.java
@@ -0,0 +1,45 @@
+package tests;
+
+import io.qameta.allure.Step;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
+import pageObjects.MainPageObject;
+
+import java.util.concurrent.TimeUnit;
+
+@DisplayName("Google tests")
+public class FirstTest {
+    private static ChromeDriver driver;
+    private static final String mainUrl = "https://www.google.com/";
+
+    @BeforeAll
+    @Step("Initializing chromeDriver")
+    public static void setUp() {
+        System.out.println("- Initializing");
+        //init chromeDriver
+        System.setProperty("webdriver.chrome.driver", "src/libs/chromedriver");
+        driver = new ChromeDriver();
+        driver.get(mainUrl);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+    }
+
+    @Test
+    @DisplayName("Google search")
+    public void firstTest() throws InterruptedException {
+        MainPageObject mainPageObject = new MainPageObject(driver);
+        Thread.sleep(3_000);
+        mainPageObject.searchInGoogle("Java",20);
+    }
+
+    @AfterAll
+    @Step("Tear down")
+    public static void tearDown() {
+        System.out.println("- Tear down");
+        driver.quit();
+    }
+}
